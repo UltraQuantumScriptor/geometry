8ca5ed8ed9a8c136e86c3f31e94df02e5236d581
UltraQuantumScriptor <ultraquantumscriptor@gmail.com>
2024-10-19 19:34:44 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="geometry.py",
+    description="A module to calculate simple geometrical calculations",
+    version="1.0.0",
+    author="QuantumScriptor",
+    include_package_data=True,
+    install_requires=[]
+)
